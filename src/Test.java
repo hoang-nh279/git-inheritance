2fc76ff5b011da11071b1cf65e6f92b03f56dbf8
hoang-nh279 <uhoangnguyen2709@gmail.com>
2025-07-10 14:04:49 +0700

    update

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        Student student = new Student();
+
+        student.setName("Alice");
+        student.setClasses("C05");
+        student.displayInfo();
+    }
+}
